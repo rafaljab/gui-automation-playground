f41672344e4cbbc4b9cf10c861f0411dd512e48c
Rafal Jablonski <jablonski.rafal@gmail.com>
2023-02-26 00:25:28 +0100

    feat: change HashRouter to BrowserRouter

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import {HashRouter as Router, Routes, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
     <React.StrictMode>
-        <Router>
+        <Router basename='/gui-automation-playground'>
             <Routes>
                 <Route path='/*' element={<App/>}/>
             </Routes>
