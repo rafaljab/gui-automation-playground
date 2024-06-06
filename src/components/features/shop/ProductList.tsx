66f59a8835626aa8771ebf30d6bf4a065a5225f2
Rafal Jablonski <jablonski.rafal@gmail.com>
2024-06-06 22:45:18 +0200

    fix: add missing key

--- src/components/features/shop/ProductList.tsx
+++ src/components/features/shop/ProductList.tsx
@@ -1,45 +1,48 @@
-import React, {ReactElement} from 'react'
-import useCart from '../../../hooks/useCart';
-import useProducts from '../../../hooks/useProducts';
-import Product from './Product';
-import {Grid, Typography} from '@mui/material';
+import React, { ReactElement } from "react";
+import useCart from "../../../hooks/useCart";
+import useProducts from "../../../hooks/useProducts";
+import Product from "./Product";
+import { Grid, Typography } from "@mui/material";
 
 const ProductList = () => {
-    const {dispatch, REDUCER_ACTIONS, cart} = useCart()
-    const {products} = useProducts()
+  const { dispatch, REDUCER_ACTIONS, cart } = useCart();
+  const { products } = useProducts();
 
-    let pageContent: ReactElement | ReactElement[] = <Grid item xs="auto">
+  let pageContent: ReactElement | ReactElement[] = (
+    <Grid item xs="auto">
       <Typography variant="body2">Loading...</Typography>
     </Grid>
+  );
 
-    if (products?.length) {
-        pageContent = products.map(product => {
-            const inCart: boolean = cart.some(item => item.id === product.id)
+  if (products?.length) {
+    pageContent = products.map((product) => {
+      const inCart: boolean = cart.some((item) => item.id === product.id);
 
-            return (
-              <Grid item xs="auto">
-                  <Product
-                    key={product.id}
-                    product={product}
-                    dispatch={dispatch}
-                    REDUCER_ACTIONS={REDUCER_ACTIONS}
-                    inCart={inCart}/>
-              </Grid>
-            )
-        })
-    }
+      return (
+        <Grid key={product.id} item xs="auto">
+          <Product
+            key={product.id}
+            product={product}
+            dispatch={dispatch}
+            REDUCER_ACTIONS={REDUCER_ACTIONS}
+            inCart={inCart}
+          />
+        </Grid>
+      );
+    });
+  }
 
-    return (
-      <Grid
-        container
-        spacing={3}
-        direction="row"
-        justifyContent="center"
-        alignItems="baseline"
-      >
-          {pageContent}
-      </Grid>
-    )
-}
+  return (
+    <Grid
+      container
+      spacing={3}
+      direction="row"
+      justifyContent="center"
+      alignItems="baseline"
+    >
+      {pageContent}
+    </Grid>
+  );
+};
 
-export default ProductList
+export default ProductList;
